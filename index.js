2086cead675883ce61f68aec074e86ad6d4c6b55
Daniel <daniel.mwangi@student.moringaschool.com>
2023-09-23 20:10:44 +0300

    committed arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+var cats =['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendCat(bosco) {
+    cats.push(bosco);
+}
+function destructivelyPrependCat(tiana){
+    cats.unshift(tiana);
+}
+function destructivelyRemoveLastCat(bosco){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(tiana){
+    cats.shift();
+}
+function appendCat(tatiana){
+    return [...cats, tatiana];
+}
+function removeFirstCat(){
+    return cats.slice(1);
+}
+function prependCat(kamaliza){
+    return [kamaliza, ...cats];
+}
+function removeLastCat(){
+    return cats.slice(0,-1);
+}
